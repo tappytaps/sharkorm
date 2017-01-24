402a8b387f3d5b1faa983f980e08b3a352f35c13
Adrian Herridge <ah@Adrians-MacBook-Pro.local>
2017-01-24 08:44:30 +0000

    .
    Signed-off-by: Adrian Herridge <ah@Adrians-MacBook-Pro.local>

---
+++ SharkORM-bridging.h
@@ -0,0 +1,14 @@
+//
+//  SharkORM-bridging.h
+//  SharkORM
+//
+//  Created by Adrian Herridge on 23/01/2017.
+//  Copyright © 2017 Adrian Herridge. All rights reserved.
+//
+
+#ifndef SharkORM_bridging_h
+#define SharkORM_bridging_h
+
+#import "SharkORM.h"
+
+#endif /* SharkORM_bridging_h */
